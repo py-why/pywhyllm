e9577382a2d6bbc7170044daafcbc64d85bac28d
Grace Sng <grace.sng75@gmail.com>
2025-03-22 00:03:01 -0500

    First draft of Simple Model Suggester testing methods.

--- pywhyllm/tests/model_suggester/example_variables.py
+++ pywhyllm/tests/model_suggester/example_variables.py
@@ -1,8 +1,58 @@
 TEST_PAIRS = {
     "test_a_cause_b": ["temperature", "ice cream sales"],
     "test_a_cause_b_expected_result": ["temperature", "ice cream sales", "The answer is <answer> A </answer>."],
-    "test_b_cause_a": ["cavities", "ice cream sales"],
-    "test_b_cause_a_expected_result": ["ice cream sales", "cavities", "The answer is <answer> B </answer>."],
-    "test_no_causality": ["ice cream sales", "shark attacks"],
+    "test_b_cause_a": ["temperature", "ice cream sales"],
+    "test_b_cause_a_expected_result": ["ice cream sales", "temperature", "The answer is <answer> B </answer>."],
+    "test_no_causality": ["temperature", "ice cream sales"],
     "test_no_causality_expected_result": [None, None, "The answer is <answer> C </answer>."],
 }
+
+TEST_VARIABLE_LISTS = {
+    "list1": ["temperature", "ice cream sales", "cavities"],
+    "list1_expected_pairwise_description": ["The answer is <answer> A </answer>.",
+                                            "The answer is <answer> C </answer>.",
+                                            "The answer is <answer> C </answer>."],
+    "list1_expected_result": {('temperature', 'ice cream sales'): "The answer is <answer> A </answer>."},
+    "list1_expected_confounders_result": ['Beach attendance', 'Water temperature',
+                                          'Availability of ice cream', 'Shark population', 'Public holidays',
+                                          'Leisure time', 'Tourist season', 'Swimming habits'],
+    "list2": ["smoking", "lung cancer", "exercise habits", "air pollution exposure"],
+    "list2_expected_pairwise_description": ["The answer is <answer> A </answer>.",
+                                            "The answer is <answer> C </answer>.",
+                                            "The answer is <answer> A </answer>.",
+                                            "The answer is <answer> C </answer>.",
+                                            "The answer is <answer> B </answer>.",
+                                            "The answer is <answer> B </answer>."],
+    "list2_expected_result": {('smoking', 'lung cancer'): "The answer is <answer> A </answer>.",
+                              ('smoking', 'air pollution exposure'): "The answer is <answer> A </answer>.",
+                              ('air pollution exposure', 'lung cancer'): "The answer is <answer> B </answer>.",
+                              ('air pollution exposure', 'exercise habits'): "The answer is <answer> B </answer>."
+                              },
+}
+
+# TEST_PAIRS = {
+#
+#     #imaginary graph with 3 nodes, "temperature", "ice cream sales" and "shark attacks"
+#     "test_a_cause_b": ["temperature", "ice cream sales"],
+#     "test_a_cause_b_expected_result": ["temperature", "ice cream sales", "The answer is <answer> A </answer>."],
+#     "test_b_cause_a": ["temperature", "ice cream sales"],
+#     "test_b_cause_a_expected_result": ["ice cream sales", "temperature", "The answer is <answer> B </answer>."],
+#     "test_no_causality": ["temperature", "ice cream sales"],
+#     "test_no_causality_expected_result": [None, None, "The answer is <answer> C </answer>."],
+#
+#     "test_a_cause_c": ["temperature", "shark attacks"],
+#     "test_a_cause_c_expected_result": ["temperature", "shark attacks", "The answer is <answer> A </answer>."],
+#     "test_c_cause_a": ["temperature", "shark attacks"],
+#     "test_c_cause_a_expected_result": ["shark attacks", "temperature", "The answer is <answer> B </answer>."],
+#     "test_no_causality": ["temperature", "shark attacks"],
+#     "test_no_causality_expected_result": [None, None, "The answer is <answer> C </answer>."],
+#
+#     "test_b_cause_c": ["ice cream sales", "shark attacks"],
+#     "test_b_cause_c_expected_result": ["ice cream sales", "shark attacks", "The answer is <answer> A </answer>."],
+#     "test_c_cause_b": ["ice cream sales", "shark attacks"],
+#     "test_c_cause_b_expected_result": ["shark attacks", "ice cream sales", "The answer is <answer> B </answer>."],
+#     "test_no_causality": ["ice cream sales", "shark attacks"],
+#     "test_no_causality_expected_result": [None, None, "The answer is <answer> C </answer>."],
+#
+#
+# }
--- pywhyllm/tests/model_suggester/test_simple_model_suggester.py
+++ pywhyllm/tests/model_suggester/test_simple_model_suggester.py
@@ -3,6 +3,8 @@ from guidance.models._openai import OpenAI
 
 from pywhyllm.suggesters.simple_model_suggester import SimpleModelSuggester
 from ...tests.model_suggester.example_variables import TEST_PAIRS as pairs
+from ...tests.model_suggester.example_variables import TEST_VARIABLE_LISTS as lists
+
 
 class TestSimpleModelSuggester(object):
 
@@ -14,17 +16,72 @@ class TestSimpleModelSuggester(object):
 
         mock_llm.__add__ = MagicMock(return_value=mock_llm)
 
-        #Given variables A and B, mock the LLM to return A->B
+        # Given variables A and B, mock the LLM to return A->B
         mock_llm.__getitem__ = MagicMock(return_value=pairs["test_a_cause_b_expected_result"][2])
         result = modeler.suggest_pairwise_relationship(pairs["test_a_cause_b"][0], pairs["test_a_cause_b"][1])
         assert result == pairs["test_a_cause_b_expected_result"]
 
-        #Given variables B and A, mock the LLM to return B->A
+        # Given variables B and A, mock the LLM to return B->A
         mock_llm.__getitem__ = MagicMock(return_value=pairs["test_b_cause_a_expected_result"][2])
         result = modeler.suggest_pairwise_relationship(pairs["test_b_cause_a"][0], pairs["test_b_cause_a"][1])
         assert result == pairs["test_b_cause_a_expected_result"]
 
-        #Given variables A and B, but LLM doesn't return a relationship, mock the LLM to return no causality
+        # Given variables A and B, mock the LLM to return no causality
         mock_llm.__getitem__ = MagicMock(return_value=pairs["test_no_causality_expected_result"][2])
+
         result = modeler.suggest_pairwise_relationship(pairs["test_no_causality"][0], pairs["test_no_causality"][1])
         assert result == pairs["test_no_causality_expected_result"]
+
+    def test_suggest_relationships_two_nodes(self):
+        modeler = SimpleModelSuggester()
+        mock_llm = MagicMock(spec=OpenAI)
+        modeler.llm = mock_llm
+
+        mock_llm.__add__ = MagicMock(return_value=mock_llm)
+
+        mock_llm.__getitem__ = MagicMock(return_value=pairs["test_a_cause_b_expected_result"][2])
+        expected_result = {(pairs["test_a_cause_b_expected_result"][0], pairs["test_a_cause_b_expected_result"][1]):
+                               pairs["test_a_cause_b_expected_result"][2]}
+        result = modeler.suggest_relationships(pairs["test_a_cause_b"])
+        assert result == expected_result
+
+        mock_llm.__getitem__ = MagicMock(return_value=pairs["test_b_cause_a_expected_result"][2])
+        expected_result = {(pairs["test_b_cause_a_expected_result"][0], pairs["test_b_cause_a_expected_result"][1]):
+                               pairs["test_b_cause_a_expected_result"][2]}
+        result = modeler.suggest_relationships(pairs["test_b_cause_a"])
+        assert result == expected_result
+
+        mock_llm.__getitem__ = MagicMock(return_value=pairs["test_no_causality_expected_result"][2])
+        result = modeler.suggest_relationships(pairs["test_no_causality"])
+        assert len(result) == 0
+
+    def test_suggest_relationships(self):
+        modeler = SimpleModelSuggester()
+        mock_llm = MagicMock(spec=OpenAI)
+        modeler.llm = mock_llm
+
+        mock_llm.__add__ = MagicMock(return_value=mock_llm)
+
+        # List of three variables
+        mock_llm.__getitem__ = MagicMock(side_effect=lists["list1_expected_pairwise_description"])
+        result = modeler.suggest_relationships(lists["list1"])
+        assert result == lists["list1_expected_result"]
+
+        # List of four variables
+        mock_llm.__getitem__ = MagicMock(side_effect=lists["list2_expected_pairwise_description"])
+        result = modeler.suggest_relationships(lists["list2"])
+        assert result == lists["list2_expected_result"]
+
+    def test_suggest_confounders(self):
+        modeler = SimpleModelSuggester()
+        mock_llm = MagicMock(spec=OpenAI)
+        modeler.llm = mock_llm
+
+        mock_llm.__add__ = MagicMock(return_value=mock_llm)
+
+        expected_result = lists["list1_expected_confounders_result"]
+        expected_result = [f"<conf>{var}</conf>" for var in expected_result]
+        expected_result = " ".join(expected_result)
+        mock_llm.__getitem__ = MagicMock(return_value=expected_result)
+        result = modeler.suggest_confounders(lists["list1"], "season", "shark attacks")
+        assert result == lists["list1_expected_confounders_result"]
