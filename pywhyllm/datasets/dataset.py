1f90a655948cefd18c16e451b7f15b2e7fa2a635
Amit Sharma <amit_sharma@live.com>
2025-07-17 14:16:47 +0200

    created base structure for adding discovery and other algorithms
    Signed-off-by: Amit Sharma <amit_sharma@live.com>

---
+++ pywhyllm/datasets/dataset.py
@@ -0,0 +1,33 @@
+from typing import List, Protocol
+
+class Dataset(Protocol):
+    
+    def graph(self):
+        """
+        Output the graph associated with the dataset. 
+
+        Args:
+        Returns:
+            A networkx graph 
+        """
+        pass
+
+    def data(self):
+        """
+        Output the data associated with the dataset.
+
+        Args:
+        Returns:
+            A numpy array containing the data
+        """
+        pass
+
+    def generate_data(self):
+        """
+        Generate synthetic data based on the dataset's causal graph.
+
+        Args:
+        Returns:
+            A numpy array containing the synthetic data
+        """
+        pass
---
+++ pywhyllm/datasets/metrics.py
@@ -0,0 +1,25 @@
+
+
+def accuracy(edges, true_edges):
+    """Accuracy metric for predicted edges
+    """
+    pass
+
+
+def precision(edges, true_edges):
+    """Precision metric for predicted edges
+    """
+    pass
+
+
+def recall(edges, true_edges):
+    """Recall metric for predicted edges
+    """
+    pass
+
+
+def F1(edges, true_edges)
+    """F1 metric for predicted edges
+    """
+    pass
+    
